35343246f9f9170e005e0b5bdc065c594054d3f7
tekuprogramming <mila.p.06@seznam.cz>
2024-03-18 10:45:56 +0100

    added new instances: int scoreX, int scoreO added new method playMultipleGames edited method playGame added new method isWinner added new method isBoardFull added new method updateScore

--- TicTacToe-master/src/TicTacToe.java
+++ TicTacToe-master/src/TicTacToe.java
@@ -3,14 +3,36 @@ import java.util.Scanner;
 public class TicTacToe {
     private static char[][] board;
     private static final int SIZE = 3;
+    private static int scoreX = 0;
+    private static int scoreO = 0;
 
     public TicTacToe() {
         initializeBoard();
     }
 
-    public void playGame() {
+    public static void playMultipleGames() {
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            initializeBoard();
+            printBoard();
+
+            playGame(scanner);
+
+            System.out.println("Player X won: " + scoreX + " times, Player O won: " + scoreO + " times");
+            System.out.println("Do you want to play again? (Y/N)");
+            String again = scanner.next();
+            if (!again.equalsIgnoreCase("Y")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void playGame(Scanner scanner) {
         char currentPlayer = 'X';
+
         while (true) {
             System.out.println("Player " + currentPlayer + ", input the position (row and column separated by a space):");
             int row = scanner.nextInt();
@@ -19,35 +41,77 @@ public class TicTacToe {
             if (isValidMove(row, col)) {
                 board[row][col] = currentPlayer;
                 printBoard();
+
+                if (isWinner(currentPlayer)) {
+                    System.out.println("Player " + currentPlayer + " won!");
+                    updateScore(currentPlayer);
+                    break;
+                }
+
+                if (isBoardFull()) {
+                    System.out.println("It's a tie!");
+                    break;
+                }
+
                 currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
             } else {
                 System.out.println("Invalid position, try again.");
             }
         }
-
-
-
     }
-
-    public void initializeBoard() {
-        board = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                board[i][j] = '-';
+        public static void initializeBoard () {
+            board = new char[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    board[i][j] = '-';
+                }
             }
         }
-    }
 
-    public void printBoard() {
+        public static void printBoard () {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.println(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static boolean isValidMove (int row, int col){
+            return row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == '-';
+        }
+
+        public static boolean isWinner(char player) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true;
+            }
+        }
+            if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+                return true;
+            }
+            return board[0][2] == player && board[1][1] == player && board[2][0] == player;
+        }
+
+        public static boolean isBoardFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                System.out.println(board[i][j] + " ");
+                if (board[i][j] == '-') {
+                    return false;
+                }
             }
-            System.out.println();
         }
-    }
+        return true;
+        }
 
-    public boolean isValidMove(int row, int col) {
-        return row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == '-';
-    }
+        public static void updateScore(char player) {
+        if (player == 'X') {
+            scoreX++;
+        } else {
+            scoreO++;
+        }
+        }
 }
