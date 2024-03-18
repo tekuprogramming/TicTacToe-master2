fa5af4a576e996a2bb4a11b34b80ae61f4161bbf
tekuprogramming <mila.p.06@seznam.cz>
2024-03-18 10:57:36 +0100

    added scoreN

--- TicTacToe-master/src/TicTacToe.java
+++ TicTacToe-master/src/TicTacToe.java
@@ -5,6 +5,7 @@ public class TicTacToe {
     private static final int SIZE = 3;
     private static int scoreX = 0;
     private static int scoreO = 0;
+    private static int scoreN = 0;
 
     public TicTacToe() {
         initializeBoard();
